0eb8c70ce3d26a17ee472194cf50846132477662
JarvisEQ <lfjarvis@gmail.com>
2020-01-23 16:02:35 -0500

    Adding project files

---
+++ src/com/company/MainWindow.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class MainWindow {
+
+    public void MainWindow(){
+        JFrame frame = new JFrame("Ye Olde Shoppe - Spring 2020");
+
+        frame.setVisible(true);
+    }
+}
