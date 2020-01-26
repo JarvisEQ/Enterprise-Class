ed3a30d59597ef43b0877dbe844200c4fcef3834
JarvisEQ <lfjarvis@gmail.com>
2020-01-26 16:44:56 -0500

    finished project 1

--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -11,8 +11,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class MainWindow extends JFrame{
@@ -20,11 +23,13 @@ public class MainWindow extends JFrame{
     String bookID;
     int quantity;
     int numBooks;
-    int processed = 0;
     int totalBooks = 0;
     int discount = 0;
     float subTotal;
     float itemCost;
+    String info;
+    int processed = 0;
+    ArrayList<String> cart = new ArrayList<String>();
     DecimalFormat df = new DecimalFormat("0.00");
     Map Order = new HashMap();
     Map names;
@@ -67,6 +72,8 @@ public class MainWindow extends JFrame{
         buttons.add(newOrder);
         buttons.add(exitStore);
 
+        //set confirm button false initially
+        confirmItem.setEnabled(false);
 
         c.add(mainPanel, BorderLayout.CENTER);
         c.add(buttons, BorderLayout.SOUTH);
@@ -85,7 +92,12 @@ public class MainWindow extends JFrame{
         processItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                checkItem();
+                
+                // if checkItem() worked properly, change button
+                if(checkItem()) {
+                    processItem.setEnabled(false);
+                    confirmItem.setEnabled(true);
+                }
             }
         });
 
@@ -94,12 +106,35 @@ public class MainWindow extends JFrame{
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 addItem();
+                confirmItem.setEnabled(false);
+
+                if(numBooks != processed){
+                    processItem.setEnabled(true);
+                }
             }
         });
 
+        //clears out orders
         newOrder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                newOrder();
+            }
+        });
+
+        //gives an alert that shows the current cart
+        viewOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                viewOrder();
+            }
+        });
+
+        //give alert for total order and clear out the order
+        finishOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                finishOrder();
                 newOrder();
             }
         });
@@ -133,27 +168,22 @@ public class MainWindow extends JFrame{
     }
 
     // checks that the Item is value and returns the values to the user
-    public void checkItem(){
+    //true if item is can move onto get part, false if failed.
+    public boolean checkItem(){
 
         bookID = mainPanel.getBookID();
         numBooks = mainPanel.getnumBooks();
         quantity = mainPanel.getbookQuantity();
 
-        // if there is no more books to add, return without appending orders
-        if(numBooks <= processed){
-            return;
-        }
-
         //if there is no quantity specified, then return
         if(quantity <= 0){
-            JOptionPane.showMessageDialog(null, "Please specify a quantity");
-            return;
+            return false;
         }
 
         //message if there is no book id found
         if(!prices.containsKey((String)bookID)){
             JOptionPane.showMessageDialog(null, "Book ID " + bookID + " not in file");
-            return;
+            return false;
         }
 
         int tmp = totalBooks + quantity;
@@ -167,12 +197,16 @@ public class MainWindow extends JFrame{
         itemCost = Float.parseFloat((String) prices.get(bookID));
         itemCost = (float) ((quantity *  itemCost) * ((100-discount) * 0.01));
 
-        String info = bookID + " " + names.get(bookID) + " " + prices.get(bookID) + " " + quantity + " " + discount + "% $" + df.format(itemCost);
+        info = bookID + " " + names.get(bookID) + " " + prices.get(bookID) + " " + quantity + " " + discount + "% $" + df.format(itemCost);
         mainPanel.setItemInfo(info);
+
+        return true;
     }
 
     public void addItem(){
 
+        cart.add(info);
+        totalBooks = quantity;
         //check if an item is already in the Order
         //if so, we're just going to append the quanity
         if(Order.containsKey(bookID)){
@@ -181,18 +215,20 @@ public class MainWindow extends JFrame{
         }
 
         Order.put(bookID, quantity);
-        processed++;
 
+        processed++;
         calculateSub();
         mainPanel.clearForNextItem();
-        JOptionPane.showMessageDialog(null, "Item #" + (processed+1) + " accepted");
+        JOptionPane.showMessageDialog(null, "Item #" + (processed) + " accepted");
     }
 
+    // clears everything out
     public void newOrder(){
 
         Order = new HashMap();
-        processed = 0;
+        cart = new ArrayList<String>();
         totalBooks = 0;
+        subTotal = 0;
         mainPanel.clearAll();
     }
 
@@ -207,13 +243,106 @@ public class MainWindow extends JFrame{
         while(orderIte.hasNext()){
             Map.Entry entry = (Map.Entry) orderIte.next();
             String key = (String) entry.getKey();
-            int value = entry.setValue();
+            int quant = (int) entry.getValue();
+            String priceString = (String) prices.get(key);
 
-            Float price = Float.parseFloat(prices.get(key));
-            subTotal += price * key;
+            Float price = Float.parseFloat(priceString);
+            subTotal += price * quant;
         }
 
-        mainPanel.setSubtotal(String.valueOf(subTotal));
+        //apply discount
+        subTotal = (float) ((0.01 * (100 - discount)) * subTotal);
+
+        mainPanel.setSubtotal(df.format(subTotal));
+    }
+
+    //append the current cart and then give it as an alert
+    public void viewOrder(){
+        String tmp = "";
+
+        for(int i = 0; i < cart.size(); i++){
+            tmp += i + ". " + cart.get(i);
+            tmp += "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, tmp);
+    }
+
+    public void finishOrder(){
+        // everything will be appended to this string
+        String tmp = "";
+
+        //EST time-zone
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss z");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("EST"));
+
+        //adding date
+        Date date = new Date();
+        tmp += "Date: " + dateFormat.format(date) + "\n\n";
+
+        // number of items added to the order
+        tmp += "Numbers of line items: " + processed + "\n\n";
+
+        // adding cart items
+        tmp += "Item# / ID / Title / Price / QTY / Disc % / Subtotal\n\n";
+        for(String item: cart){
+            tmp += item + "\n";
+        }
+
+        //sub total amount
+        tmp += "\n\nOrder subtotal: " + df.format(subTotal) + "\n\n";
+
+        //tax rate is hard-coded, this probably won't work IRL
+        tmp += "Tax rate: 6%\n\n";
+
+        //adding tax amount
+        double tax = subTotal * 0.06;
+        tmp += "Tax amount: " + df.format(tax) + "\n\n";
+
+        //adding total
+        double total = tax + subTotal;
+        tmp += "Order Total: " + df.format(total) + "\n\n";
+
+        //giving final message as a alert
+        tmp += "Thank you for shopping at the Ye Olde Book Shoppe!";
+
+        writeToFile();
+        JOptionPane.showMessageDialog(null, tmp);
+    }
+
+    //writes items in the cart to the transaction files
+    public void writeToFile(){
+
+        //find
+        String file = "transactions.txt";
+        Date date = new Date();
+
+        //Date formatting
+        SimpleDateFormat frontDate = new SimpleDateFormat("ddMMyyyyHHmm");
+        SimpleDateFormat backDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss z");
+        frontDate.setTimeZone(TimeZone.getTimeZone("EST"));
+        backDate.setTimeZone(TimeZone.getTimeZone("EST"));
+
+        //these dates will be the same across all items in the cart
+        String front = frontDate.format(date);
+        String back = backDate.format(date);
+
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.txt", true));
+
+            //add items from the cart to the transaction file
+            for(String entry: cart){
+                String tmp = front + ", " + entry + ", " + back;
+                writer.newLine();
+                writer.write(tmp);
+            }
+
+            writer.close();
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
 
--- src/com/company/Order.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-//TODO implement an Order class to help with this fucking program
-public class Order {
-    int numBooks;
-    int bookID;
-
-}
